30b4114780b2f29e591c921773e4c9e5fd94fbfa
SyvArtur <121033102+SyvArtur@users.noreply.github.com>
2023-10-10 14:47:00 +0300

    Uploaded the game to GitHub

---
+++ Sudoku/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Sudoku
+{
+    partial class Game_Sudoku
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.cmb_box = new System.Windows.Forms.ComboBox();
+            this.btn_check = new System.Windows.Forms.Button();
+            this.lvl = new System.Windows.Forms.Label();
+            this.examination = new System.Windows.Forms.Label();
+            this.lblSaveImage = new System.Windows.Forms.Label();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 200;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // cmb_box
+            // 
+            this.cmb_box.DisplayMember = "Легкий";
+            this.cmb_box.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cmb_box.FormattingEnabled = true;
+            this.cmb_box.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.cmb_box.Items.AddRange(new object[] {
+            "Легкий",
+            "Нормальний",
+            "Важкий",
+            "Для теста"});
+            this.cmb_box.Location = new System.Drawing.Point(147, 12);
+            this.cmb_box.Name = "cmb_box";
+            this.cmb_box.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cmb_box.Size = new System.Drawing.Size(121, 28);
+            this.cmb_box.TabIndex = 1;
+            this.cmb_box.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btn_check
+            // 
+            this.btn_check.BackColor = System.Drawing.Color.LightGray;
+            this.btn_check.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_check.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btn_check.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btn_check.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_check.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(204)));
+            this.btn_check.Location = new System.Drawing.Point(697, 12);
+            this.btn_check.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_check.Name = "btn_check";
+            this.btn_check.Size = new System.Drawing.Size(27, 28);
+            this.btn_check.TabIndex = 2;
+            this.btn_check.UseVisualStyleBackColor = false;
+            this.btn_check.Click += new System.EventHandler(this.btn_check_Click);
+            // 
+            // lvl
+            // 
+            this.lvl.Location = new System.Drawing.Point(26, 17);
+            this.lvl.Name = "lvl";
+            this.lvl.Size = new System.Drawing.Size(100, 23);
+            this.lvl.TabIndex = 3;
+            this.lvl.Text = " Рівень:";
+            // 
+            // examination
+            // 
+            this.examination.Location = new System.Drawing.Point(516, 12);
+            this.examination.Name = "examination";
+            this.examination.Size = new System.Drawing.Size(178, 31);
+            this.examination.TabIndex = 4;
+            this.examination.Text = "Автоперевірка помилок :";
+            // 
+            // lblSaveImage
+            // 
+            this.lblSaveImage.Location = new System.Drawing.Point(494, 270);
+            this.lblSaveImage.Name = "lblSaveImage";
+            this.lblSaveImage.Size = new System.Drawing.Size(334, 31);
+            this.lblSaveImage.TabIndex = 5;
+            this.lblSaveImage.Text = "(зберігає в тій же папці, де знаходиться виконуваний файл) ";
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.BackColor = System.Drawing.Color.LightGray;
+            this.btnSaveImage.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSaveImage.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnSaveImage.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gainsboro;
+            this.btnSaveImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(204)));
+            this.btnSaveImage.Location = new System.Drawing.Point(558, 242);
+            this.btnSaveImage.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(191, 28);
+            this.btnSaveImage.TabIndex = 6;
+            this.btnSaveImage.Text = "Зберегти зображення";
+            this.btnSaveImage.UseVisualStyleBackColor = false;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // Game_Sudoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(885, 431);
+            this.Controls.Add(this.btnSaveImage);
+            this.Controls.Add(this.lblSaveImage);
+            this.Controls.Add(this.examination);
+            this.Controls.Add(this.lvl);
+            this.Controls.Add(this.btn_check);
+            this.Controls.Add(this.cmb_box);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 39);
+            this.Name = "Game_Sudoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ComboBox cmb_box;
+        private System.Windows.Forms.Button btn_check;
+        private System.Windows.Forms.Label lvl;
+        private System.Windows.Forms.Label examination;
+        private System.Windows.Forms.Label lblSaveImage;
+        private System.Windows.Forms.Button btnSaveImage;
+    }
+}
+
---
+++ Sudoku/MainForm.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+
+namespace Sudoku
+{
+    public partial class Game_Sudoku : Form
+    {
+        public Game_Sudoku()
+        {
+            InitializeComponent();
+        }
+
+
+        int[,] sudoku;
+        SolidBrush black = new SolidBrush(Color.Black);
+        Button[,] _buttons = new Button[9, 9];
+        Button[] key = new Button[10];
+        Random rnd = new Random();
+        int difficult;
+        bool check=false;
+        // double proportion = 0.58;
+
+
+        private void difficult_start(int difficult)
+        {
+            int x, y;
+            sudoku = GenSudoku.start_gen(rnd);
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    _buttons[i, j].Text = sudoku[i, j].ToString();
+                    _buttons[i, j].Cursor = Cursors.Default;
+                    _buttons[i, j].ForeColor = Color.Black;
+                }
+            }
+
+            for (int i = 0; i < difficult; i++)
+            {
+                x = rnd.Next(0, 9);
+                y = rnd.Next(0, 9);
+                _buttons[x, y].Text = "";
+                _buttons[x, y].Cursor = Cursors.Hand;
+            }
+
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var button = new Button();
+                    string btn_name = ""+i + j;
+                    //button.BackColor = Color.Gray;
+                    //button.Tag = j * 9 + i;
+                    button.FlatAppearance.BorderColor = Color.Gainsboro;
+                    button.Click += Btn_Click;
+                    button.Name = btn_name;
+                    _buttons[i, j] = button;
+                    this.Controls.Add(button);
+                }
+                
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var button_key = new Button();
+                if (i == 9)
+                {
+                    button_key.Text = "Очистити";
+                } else { button_key.Text = (i + 1).ToString(); }
+                button_key.Click += Btn_key_Click;
+                button_key.Name = (i + 1).ToString();
+                button_key.FlatStyle = FlatStyle.Flat;
+                button_key.FlatAppearance.BorderColor = Color.Gainsboro;
+               // button.FlatAppearance = 
+                button_key.BackColor = Color.Gainsboro;
+                button_key.ForeColor = Color.Blue;
+                key[i] = button_key;
+                this.Controls.Add(button_key);
+            }
+            difficult_start(50);
+            cmb_box.SelectedItem = "Легкий";
+            this.Size = new Size(800, (int)(506 + 35));
+        }
+
+
+
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+            check = !check;
+            property();
+            if (check)
+            {
+                btn_check.Text = "✔";
+            } else
+                btn_check.Text = "";
+        }
+
+        private void property()
+        {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (!_buttons[i, j].Text.Equals(sudoku[i, j].ToString()) && _buttons[i, j].Cursor == Cursors.Hand && check)
+                        {
+                            _buttons[i, j].ForeColor = Color.Red;
+                        }
+                    else if (_buttons[i, j].Cursor == Cursors.Hand)
+                    {
+                        _buttons[i, j].ForeColor = Color.Blue;
+                    }
+                    }
+                }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Btn_Click(object sender, EventArgs eventArgs)
+        {
+            var button = (Button)sender;
+            if (button != null && button.Cursor==Cursors.Hand)
+            {
+                button.Text = press_key;
+                property();
+                bool win = true;
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        
+                        if (!_buttons[i, j].Text.Equals (sudoku[i, j].ToString())  )
+                            {
+                                win = false;
+                            }
+                    }
+                }
+              
+                if (win)
+                {
+                    MessageBox.Show("You win!!!");
+                }
+            }
+               
+                
+            }
+
+        String press_key;
+        private void Btn_key_Click(object sender, EventArgs eventArgs)
+        {
+            var button = (Button)sender;
+            for (int i = 0; i < 10; i++)
+            {
+              key[i].BackColor = Color.Gainsboro;
+            }
+            button.BackColor = Color.Silver;
+            //MessageBox.Show(button.Text);
+            if(button.Text=="Очистити")
+            {
+                press_key = "";
+            } else
+                press_key = button.Text;
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int x = this.Size.Width;
+            if (key[0].Width != x / 10)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+
+                        _buttons[i, j].Width = x / 16;
+                        _buttons[i, j].Height = x / 16;
+                        _buttons[i, j].Font = new Font("Microsoft Sans Serif", x / 50);
+                        _buttons[i, j].Location = new Point(_buttons[i, j].Width * i + x / 200 * Convert.ToInt32(i / 3) + x / 200, (_buttons[i, j].Height * j + x / 20) + x / 220 * Convert.ToInt32(j / 3) + x / 50);
+
+                    }
+                    // key
+                    key[i].Width = x / 10;
+                    key[i].Height = x / 10;
+                    key[i].Font = new Font("Microsoft Sans Serif", (float)x / 35);
+                    key[i].Location = new Point(key[i].Width * (i % 3) * 9 / 8 + _buttons[i, 1].Width * 10, key[i].Height * Convert.ToInt32(i / 3) * 8 / 7 + _buttons[i, 1].Width * 2);
+                }
+
+
+                key[9].Width = x / 7;
+                key[9].Height = x / 10;
+                key[9].Font = new Font("Microsoft Sans Serif", (float)x / 60);
+                key[9].Location = new Point(x * 143 / 200, x / 30);
+                System.Drawing.Drawing2D.GraphicsPath Button_Path = new System.Drawing.Drawing2D.GraphicsPath();
+                Button_Path.AddEllipse(0, this.key[9].Width/9, this.key[9].Width*98/100, this.key[9].Height*2/3);
+                Region Button_Region = new Region(Button_Path);
+                this.key[9].Region = Button_Region;
+
+
+                cmb_box.Width = x / 7;
+                cmb_box.Location = new Point(x / 8, x / 70);
+                cmb_box.Font = new Font("Microsoft Sans Serif", (float)x / 60);
+
+                lvl.Width = x / 9;
+                lvl.Height = x / 25;
+                lvl.Location = new Point(x / 70, x / 60);
+                lvl.Font = new Font("Microsoft Sans Serif", (float)x / 60);
+
+                btn_check.Width = x / 30;
+                btn_check.Height = x / 30;
+                btn_check.Location = new Point(x * 30 / 48, x / 60);
+                btn_check.Font = new Font("Microsoft Sans Serif", (float)x / 80);
+
+                examination.Height = x / 25;
+                examination.Width = x * 33 / 120;
+                examination.Location = new Point(x / 3, x / 60);
+                examination.Font = new Font("Microsoft Sans Serif", (float)x / 60);
+
+                lblSaveImage.Height = x / 25;
+                lblSaveImage.Width = x / 2;
+                lblSaveImage.Location = new Point(x * 3 / 5, x * 9 / 16);
+                lblSaveImage.Font = new Font("Microsoft Sans Serif", (float)x / 110);
+
+                btnSaveImage.Height = x / 25;
+                btnSaveImage.Width = x / 4;
+                btnSaveImage.Location = new Point(x * 70 / 105, x * 4 / 8);
+                btnSaveImage.Font = new Font("Microsoft Sans Serif", (float)x / 80);
+            }
+                Graphics g = CreateGraphics();
+                g.Clear(Color.WhiteSmoke);
+                g.FillRectangle(black, 0, x / 16, x / 16 * 9 + x / 200 * 4, x);
+            
+        }
+
+
+        
+       private void Form1_Resize(object sender, EventArgs e)
+        {
+            int x = this.Size.Width;
+
+            Control control = (Control)sender;
+
+                if (control.Size.Height != (int)(x / 16 * 9 + x / 50 +35+ x / 15))
+                {
+                    control.Size = new Size(control.Size.Width, (int)(x / 16 * 9 + 35 + x / 50 +x/15));
+                }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string selectedState = cmb_box.SelectedItem.ToString();
+            if (selectedState=="Легкий")
+            {
+                difficult = 50;
+            }
+            if (selectedState == "Нормальний")
+            {
+                difficult = 75;
+            }
+            if (selectedState == "Важкий")
+            {
+                difficult = 100;
+            }
+            if (selectedState == "Для теста")
+            {
+                difficult = 3;
+            }
+            difficult_start(difficult);
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            int x = this.Size.Width;
+            Bitmap bmpScreenshot = new Bitmap(x / 16 * 9 + x / 200 * 4, x / 16 * 9 + x / 200 * 4, PixelFormat.Format32bppArgb);
+
+            Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            gfxScreenshot.CopyFromScreen(this.Location.X+8, this.Location.Y+(x/16)+31, 0, 0, new Size (x / 16 * 9 + x / 200 * 4, x / 16 * 9 + x / 200 * 4), CopyPixelOperation.SourceCopy);
+
+            bmpScreenshot.Save("screenshot.png", ImageFormat.Png);
+            MessageBox.Show("Зображення screenshot.png завантажилось по шляху: " +  Environment.CurrentDirectory);
+        }
+    }
+
+    public static class GenSudoku 
+    {
+        static int[,] sudoku = new int[9, 9];
+
+        public static bool check_sector(int x, int y)
+        {
+            for (int i = 3 * Convert.ToInt32(x / 3); i < (3 * Convert.ToInt32(x / 3) + 3); i++)
+            {
+                for (int j = 3 * Convert.ToInt32(y / 3); j < (3 * Convert.ToInt32(y / 3) + 3); j++)
+                {
+                    if ((i != x) & (j != y))
+                    {
+                        if (sudoku[x, y] == sudoku[i, j])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool check_column(int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != y)
+                {
+                    if (sudoku[x, y] == sudoku[x, i])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool check_line(int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != x)
+                {
+                    if (sudoku[x, y] == sudoku[i, y])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool gen(Random rnd)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    sudoku[i, j] = rnd.Next(1, 10);
+                    int tim = 0;
+                    while (check_column(i, j) | check_line(i, j) | check_sector(i, j))
+                    {
+                        tim++;
+                        sudoku[i, j] = rnd.Next(1, 10);
+                        if (tim == 30) { return true; }
+                    }
+
+                }
+            }
+            return false;
+        }
+
+        public static int[,] start_gen(Random rnd)
+        {
+            while (gen(rnd))
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        sudoku[i, j] = 0;
+                    }
+                }
+            }
+            return sudoku;
+        }
+    }
+
+}
